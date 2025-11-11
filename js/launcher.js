ebc4b4a2de82e04d0debb11c142b9f0890828477
viggle-by <barlevsj@gmail.com>
2025-11-11 17:42:18 -0500

    Update launcher.js

--- js/launcher.js
+++ js/launcher.js
@@ -1,29 +1,29 @@
 var CLIENTS = {
-  "eaglercraft-1.12.2-js":"/versions/eaglercraft/1.12.2/js/index.html",
-  "eaglercraft-1.12.2-wasm":"/versions/eaglercraft/1.12.2/wasm/index.html",
-  "eaglercraft-1.8.8-js":"/versions/eaglercraft/1.8.8/js/",
-  "eaglercraft-1.8.8-wasm":"/versions/eaglercraft/1.8.8/wasm/",
-  "eaglercraft-1.5.2-js":"/versions/eaglercraft/1.5.2/index.html",
+  "eaglercraft-1.12.2-js":"/launcher-v2/versions/eaglercraft/1.12.2/js/index.html",
+  "eaglercraft-1.12.2-wasm":"/launcher-v2/versions/eaglercraft/1.12.2/wasm/index.html",
+  "eaglercraft-1.8.8-js":"/launcher-v2/versions/eaglercraft/1.8.8/js/",
+  "eaglercraft-1.8.8-wasm":"/launcher-v2/versions/eaglercraft/1.8.8/wasm/",
+  "eaglercraft-1.5.2-js":"/launcher-v2/versions/eaglercraft/1.5.2/index.html",
   "eaglercraft-1.8.8-u54":"https://stoppedwummpython.github.io/eageag/eaglercraft_js_client/",
   "eaglercraft-1.8.8-u54-wasm":"https://stoppedwummpython.github.io/eageag/eaglercraft_wasm_client/",
-  "eaglercraft-1.8.8-u53":"/versions/eaglercraft/1.8.8/js/",
-  "pixelclient-v3":"/versions/pixelclient/",
+  "eaglercraft-1.8.8-u53":"/launcher-v2/versions/eaglercraft/1.8.8/js/",
+  "pixelclient-v3":"/launcher-v2/versions/pixelclient/",
   "eaglercraft-1.8.8-u41":"https://webv0.vercel.app/website-data/prx-177/frontend.html",
   "eaglercraft-1.8.8-u31":"https://math-lessons.vercel.app/test.html",
   "astra-1.6":"https://astraclientorg.github.io/play/js/18/",
-  "astra-1.5":"/versions/astraclient/15/astra-1.5.0.html",
-  "astra-1.4":"/versions/astraclient/14/astra-1.4.0.html",
-  "astra-1.3":"/versions/astraclient/13/astraclient-1.3.0.html",
-  "astra-1.2":"/versions/astraclient/12/astra1.2.html",
+  "astra-1.5":"/launcher-v2/versions/astraclient/15/astra-1.5.0.html",
+  "astra-1.4":"/launcher-v2/versions/astraclient/14/astra-1.4.0.html",
+  "astra-1.3":"/launcher-v2/versions/astraclient/13/astraclient-1.3.0.html",
+  "astra-1.2":"/launcher-v2/versions/astraclient/12/astra1.2.html",
   "messcraft":"https://mess.eu.org/",
   "resent4.0":"https://client.eaglercraft.win/eagler-files/1.8/resent4.0/index.html"
 };
 
 var TEXTURES = {
-  "rubygigglepack":"/packs/rubygigglepack/rubygigglepack.zip",
-  "overgrowngigglepack":"/packs/overgrowngigglepack/overgrowngigglepack.zip",
-  "mysticgigglepack":"/packs/mysticgigglepack/mysticgigglepack.zip",
-  "redgigglebluepack":"/packs/redgigglebluepack/redgigglebluepack.zip"
+  "rubygigglepack":"/launcher-v2/packs/rubygigglepack/rubygigglepack.zip",
+  "overgrowngigglepack":"/launcher-v2/packs/overgrowngigglepack/overgrowngigglepack.zip",
+  "mysticgigglepack":"/launcher-v2/packs/mysticgigglepack/mysticgigglepack.zip",
+  "redgigglebluepack":"/launcher-v2/packs/redgigglebluepack/redgigglebluepack.zip"
 };
 
 var toggleBtn = document.getElementById("toggle-updates");
